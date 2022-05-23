ee989ab95ac8b8ea4f4d55e289af3b8e0d0b5367
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2022-05-23 16:51:24 -0300

    fix(search): find by whole string

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,15 +2,11 @@ module Api
   module V1
     class SearchController < V1Controller
       def index
-        related_words = SearchService.call(search_value: value_for_search)
+        related_words = SearchService.call(search_value: search_params[:value])
         render json: related_words
       end
 
       private
-
-        def value_for_search
-          search_params[:value]&.split&.first
-        end
 
         def search_params
           params.permit(:value)
